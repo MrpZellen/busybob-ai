a878a148a4b05913912a86f10728c144c9b1f5e6
zellency <Ben@benleonard.net>
2025-07-03 17:56:28 -0600

    All the docker files running my venv and Docker compose.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI;
 from dotenv import load_dotenv
-import os
+import os, pymongo
 from huggingface_hub import login
 from langchain_huggingface import HuggingFacePipeline, HuggingFaceEndpoint, ChatHuggingFace
 from langchain_core.messages import HumanMessage, SystemMessage
 load_dotenv()
 # huggingface signin process
 HF_TOKEN = os.getenv("HF_TOKEN")
-if not HF_TOKEN:
+MONGODB_URI = os.getenv("MONGODB_URI")
+if not HF_TOKEN or not MONGODB_URI:
     raise ValueError("HF_TOKEN is missing from environment variables")
 
 login(token=HF_TOKEN)
@@ -22,8 +23,21 @@ llm = HuggingFaceEndpoint(
     do_sample=False,
 )
 
+async def pymongtest():
+    print('reached pytest')
+    client = pymongo.MongoClient(MONGODB_URI)
+    db = client.survey_data
+    collection = db.responses
+    if client:
+        return True
+    else:
+        return False
+
+
 @app.get("/")
 async def root():
     llmBIG = ChatHuggingFace(llm=llm)
-    result = await llmBIG.ainvoke("you are a mascot named busybob whose catchphrase is rise and grind. Introduce yourself! The user who you are talking to is inserted below, make a comment about their name and what you like about it! username is tester john")
-    return {"message": result.content}
+    testerMong = await pymongtest()
+    result = await llmBIG.ainvoke("you are a mascot named busybob whose catchphrase is rise and grind (he's a little confused but has the spirit). Introduce yourself! The user who you are talking to is inserted below, make a comment about their name and what you like about it! username is tester john")
+    return {"message": result.content,
+            "connectedToMongo": testerMong}
